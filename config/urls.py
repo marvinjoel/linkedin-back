d0c56366c7473675198e3ec1ccdc5ce4ee75c53a
Marck <marvin.roses@gmail.com>
2022-11-01 15:44:48 -0500

    FEAT: Reset password

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('auth/', include('accounts.urls')),
 
     path('swagger<format>.json|.yaml', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
